8352aa20882c13991d09f96a7498c678bbeb844a
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2020-03-16 20:15:59 -0500

    secondCommit

---
+++ hello.py
@@ -0,0 +1,14 @@
+first_name = input("What is your first name?   ")
+
+print("Hello," , first_name)
+if first_name == "Craig":
+    print(first_name, "is learning Python")
+elif first_name == "Maximiliane":
+    print(first_name, "is learning with fellow students in the Community!  Me too!")
+else:
+    # this is just in case we have a younger user who can't yet read
+    age = int(input("How old are you?  "))
+    if age <= 6:
+        print("Wow you're {}: If you're confident with your reading already...".format(age))
+    print("You should totally learn Python, {}!".format(first_name))    
+print("Have a great day {}!".format(first_name))    
